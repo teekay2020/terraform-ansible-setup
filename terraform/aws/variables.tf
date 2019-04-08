fc598fd663a2aae3ee935998c198ee9b9a43ab68
Tokunbo <tokunbo@ip-172-28-72-131.eu-west-1.compute.internal>
2019-04-08 10:19:10 +0100

    terraform-ansible-set-up

---
+++ terraform/aws/variables.tf
@@ -0,0 +1,15 @@
+variable "access_key" {
+    description = "The AWS access key."
+    default = ""
+}
+
+variable "secret_key" {
+    description = "The AWS secret key."
+    default = ""
+}
+
+variable "region" {
+    description = "The AWS region to create resources in."
+    default = "ap-southeast-2"
+}
+
